2bae9e0776d5374c31796813f6a771b2933a19a2
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2024-08-24 16:43:44 +0200

    feat: implement bracket validation function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const openBrackets = [];
+  const closeBrackets = new Map();
+  const sameBrackets = new Map();
+  const stack = [];
+
+  bracketsConfig.forEach(([open, close]) => {
+    openBrackets.push(open);
+    closeBrackets.set(close, open);
+    if (open === close) {
+      sameBrackets.set(open, close);
+    }
+  });
+
+  for (let char of str) {
+    if (sameBrackets.has(char)) {
+      if (stack.length && stack[stack.length - 1] === char) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else if (openBrackets.includes(char)) {
+      stack.push(char);
+    } else if (closeBrackets.has(char)) {
+      if (stack.pop() !== closeBrackets.get(char)) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
